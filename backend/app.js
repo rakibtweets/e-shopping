7ab63e762901fdcb8c10f22a91d0b86897faea67
Rakib Hasan <rakibhasanroky0@gmail.com>
2022-11-28 21:14:35 +0600

    controller/routes setup

--- backend/app.js
+++ backend/app.js
@@ -7,4 +7,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// import all the routes
+const products = require('./routes/productRoutes');
+
+// use routes
+app.use('/api/v1', products);
+
 module.exports = app;
